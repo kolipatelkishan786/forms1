875a464ebe6801e8759509dfe4738ad27931e8b1
Kishan Patel <kolipatelkishan786@gmail.com>
2019-03-07 09:05:08 +0530

    199 Reactive Setting and Patching Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,11 +27,25 @@ export class AppComponent implements OnInit {
     // );
     this.signupForm.statusChanges.subscribe(
       (status) => console.log(status)
-  );
+    );
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Max',
+        'email': 'max@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+    this.signupForm.patchValue({
+      'userData': {
+        'username': 'Anna',
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
